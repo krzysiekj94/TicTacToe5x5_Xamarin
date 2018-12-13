4fafb4a7b4d7613cc0a36abefe71bbdd4fcd8e7d
krzysiekj94 <k.jerzynski1@gmail.com>
2018-12-13 23:44:42 +0100

    - prepare game view

--- TicTacToeXamarin/MainActivity.cs
+++ TicTacToeXamarin/MainActivity.cs
@@ -20,9 +20,6 @@ namespace TicTacToeXamarin
 
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
-
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
