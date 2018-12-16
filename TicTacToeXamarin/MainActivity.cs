d91578df6d1447030801b2e386924b0acbbf0076
krzysiekj94 <k.jerzynski1@gmail.com>
2018-12-16 01:20:20 +0100

    - set bitmap in ImageButton

--- TicTacToeXamarin/MainActivity.cs
+++ TicTacToeXamarin/MainActivity.cs
@@ -1,25 +1,81 @@
 ﻿using System;
+using System.Collections.Generic;
 using Android.App;
+using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Support.Design.Widget;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using TicTacToeXamarin.Game;
 
 namespace TicTacToeXamarin
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private TableLayout _gameBoardtableLayout;
+        private Dictionary<int, GameButtonStates> _gameBoard;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
-
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
+            InitBoard();
+        }
+
+        private void InitBoard()
+        {
+            TableRow gameTableRow = null;
+            Context context = Android.App.Application.Context;
+            _gameBoardtableLayout = FindViewById<TableLayout>( Resource.Id.boardTableLayout );
+            _gameBoard = new Dictionary<int, GameButtonStates>();
+
+            int id = 0;
+
+            for (int i = 0; i < _gameBoardtableLayout.ChildCount; i++)
+            {
+                View view = _gameBoardtableLayout.GetChildAt(i);
+                TableRow viewRow = null;
+
+                if(view != null && ( view is TableRow ) )
+                {
+                    viewRow = (TableRow)view;
+                    for( int j=0; j < viewRow.ChildCount; j++)
+                    {
+                        ImageButton imageButton = (ImageButton)viewRow.GetChildAt(j);
+                        imageButton.Id = id;
+                        _gameBoard.Add(id, GameButtonStates.Standard);
+                        id++;
+                    }
+                }
+            }
+
+            foreach( var gameElement in _gameBoard )
+            {
+                ImageButton imageButton = (ImageButton)_gameBoardtableLayout.FindViewById(gameElement.Key);
+
+                if(imageButton != null)
+                {
+                    switch (gameElement.Value)
+                    {
+                        case GameButtonStates.Circle:
+                            imageButton.SetImageResource(Resource.Mipmap.circle);
+                        break;
+                        case GameButtonStates.Cross:
+                            imageButton.SetImageResource(Resource.Mipmap.cross);
+                            break;
+                        case GameButtonStates.Standard:
+                            imageButton.SetImageResource(Resource.Mipmap.ic_launcher);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -37,13 +93,6 @@ namespace TicTacToeXamarin
             }
 
             return base.OnOptionsItemSelected(item);
-        }
-
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
         }
 	}
 }
